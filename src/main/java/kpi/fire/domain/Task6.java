ecdce18cf5945711647797397361ca7f15f8dcc5
Vitaly <akaredsc2@gmail.com>
2016-07-11 18:36:34 +0300

    Update Task6 report for different heat flow densities

--- src/main/java/kpi/fire/domain/Task6.java
+++ src/main/java/kpi/fire/domain/Task6.java
@@ -29,8 +29,16 @@ public class Task6 implements ReportableTask {
         builder.append(description).append(System.lineSeparator())
                 .append(FireStats.computeFireStats(data).getFireKind().toUkrString()).append(System.lineSeparator());
         if (fireKind == FireKind.VENTILATION_REGULATED) {
+            double densityOfHeatFlow = computeMaxDensityOfHeatFlow();
             builder.append("Максимальна щільність теплового потоку з продуктами горіння, які йдуть через пройоми: ")
-                    .append(new Formatter().format("%.2f",computeMaxDensityOfHeatFlow()) + " кВт/м2.");
+                    .append(new Formatter().format("%.2f", densityOfHeatFlow) + " кВт/м2.");
+            if (densityOfHeatFlow < 42.0 ) {
+                builder.append("Загалом переносимо, можливі почервонніння шкіри");
+            } else if (densityOfHeatFlow >= 42.0 && densityOfHeatFlow < 84.0) {
+                builder.append("Переносимо, висока ймовірність появи пухирів на тілі");
+            } else {
+                builder.append("Не переносимо, при ураженні більше ніж 50% шкіри настає летальний випадок");
+            }
         } else {
             builder.append("У ГОСТ Р 12.3.047-98 відсутня інформація для даної ситуації");
         }
